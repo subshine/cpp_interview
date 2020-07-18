3e1f64a9e8757dfc7acec284185a827c98251bdc
subshine <yg19960916@163.com>
2020-07-18 17:48:50 +0800

    second

--- test.cpp
+++ test.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 using namespace std;
-int main(){
-
-
-
-
+int main() { 
+    std::cout << "hello world " << std::endl;
+    
+    
     
 }
---
+++ test.h
@@ -0,0 +1,9 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: sunshine
+ * @Github: https://subshine.github.io/
+ * @Email: 2182216077@ncepu.edu.cn
+ * @Date: 2020-07-18 17:43:23
+ * @LastEditTime: 2020-07-18 17:43:24
+ */
